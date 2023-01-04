57e1c46b95c942eb0f388c4a5b2f870f23b4287c
nadiahass <hassouninadia20@gmail.com>
2023-01-04 23:48:16 +0100

    add fragment design

--- app/src/main/java/com/example/todo/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/example/todo/ui/notes/NotesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.todo.R
 import com.example.todo.databinding.FragmentAddBinding
 import com.example.todo.databinding.FragmentNotesBinding
@@ -17,6 +18,10 @@ class NotesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNotesBinding.inflate(inflater, container, false)
+
+        binding.fabAddNote.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_notesFragment_to_addFragment)
+        }
 
         return binding.root
     }
